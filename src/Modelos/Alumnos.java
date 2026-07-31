26ff8bc3baa6e513cf813274a5fa92418e499255
Revennat <25420033@uttcampus.edu.mx>
2026-07-31 16:28:20 -0600

    Cree el archivo de alumnos, dandole los atributos que estan en la base de datos

---
+++ src/Modelos/Alumnos.java
@@ -0,0 +1,12 @@
+package Modelos;
+
+public class Alumnos {
+    private int id;
+    private int matricula;
+    private String nombre;
+    private int edad;
+    private String sexo;
+    private String correo;
+
+
+}
