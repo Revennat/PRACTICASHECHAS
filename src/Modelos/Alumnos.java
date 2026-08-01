a2bd7674a02f0a764b6034b52a0f64f5eabba21d
Revennat <25420033@uttcampus.edu.mx>
2026-07-31 23:48:22 -0600

    hice varios metodos para la clase alumnos, en otras palabras, hice el metodo save, deletebymatricula, updatebymatricula, y getall

--- src/Modelos/Alumnos.java
+++ src/Modelos/Alumnos.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Alumnos {
     private int id;
@@ -13,7 +15,16 @@ public class Alumnos {
     private String sexo;
     private String correo;
 
-    public Alumnos(int matricula, String nombre, int edad, String sexo, String correo) throws Exception {
+    private Alumnos(int id, int matricula, String nombre, int edad, String sexo, String correo) {
+        this.id = id;
+        this.matricula = matricula;
+        this.nombre = nombre;
+        this.edad = edad;
+        this.sexo = sexo;
+        this.correo = correo;
+    }
+
+    private Alumnos(int matricula, String nombre, int edad, String sexo, String correo) throws Exception {
         this.matricula = matricula;
         this.nombre = nombre;
         this.edad = edad;
@@ -38,4 +49,44 @@ public class Alumnos {
             }
         }
     }
+
+    public int deleteByMatricula() throws Exception{
+        try (
+                Connection con = Conexion.getConexion();
+                PreparedStatement ps = con.prepareStatement("DELETE ALUMNOS WHERE matricula = ?")
+                ){
+            ps.setInt(1, this.matricula);
+            return ps.executeUpdate();
+        }
+    }
+
+    public int updateByMatricula() throws Exception{
+        try (
+                Connection con = Conexion.getConexion();
+                PreparedStatement ps = con.prepareStatement("UPDATE ALUMNOS SET nombre = ?, edad = ?, sexo = ?, correo = ? WHERE matricula = ?")
+                ){
+            ps.setString(1, this.nombre);
+            ps.setInt(2, this.edad);
+            ps.setString(3, this.sexo);
+            ps.setString(4, this.correo);
+            ps.setInt(5, this.matricula);
+            return ps.executeUpdate();
+        }
+    }
+
+    public static List<Alumnos> getAll() throws Exception{
+        try (
+                Connection con = Conexion.getConexion();
+                PreparedStatement ps = con.prepareStatement("SELECT * FROM ALUMNOS")
+        ){
+            ResultSet rs = ps.executeQuery();
+            List<Alumnos> listAlum = new ArrayList<>();
+            while (rs.next()){
+                Alumnos al = new Alumnos(rs.getInt("id"), rs.getInt("matricula"), rs.getString("nombre"), rs.getInt("edad"), rs.getString("sexo"), rs.getString("correo"));
+                listAlum.add(al);
+            }
+            return listAlum;
+        }
+    }
+
 }
