44a25edc6a6ef7a15852c463487fa5ce20c56a57
Revennat <25420033@uttcampus.edu.mx>
2026-08-06 11:03:47 -0600

    cree la clase animal, conexion, persona y animal, estando conexion vacia

---
+++ src/Animal.java
@@ -0,0 +1,30 @@
+public class Animal {
+    private int id;
+    private String nombre;
+    private String tipo;
+    private int persona_id;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getPersona_id() {
+        return persona_id;
+    }
+
+    public void setPersona_id(int persona_id) {
+        this.persona_id = persona_id;
+    }
+}
